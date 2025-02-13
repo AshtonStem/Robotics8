180c5123362c69a3cec08b4c0c05d868161986dc
AshtonStem <ashtonh908@stemcollegiate.ca>
2025-02-13 09:44:11 -0700

    Feb13.py

---
+++ Feb13.py
@@ -0,0 +1,11 @@
+# today i learned
+# challenge:
+
+print("Hello Replit")
+print("6 7")
+print("kansas city chiefs")
+print("I am signing up for Replit's 100 days of Python challenge!")
+print("I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.")
+print("I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!")
+
+# I learned how to properly use the print variable on python code
